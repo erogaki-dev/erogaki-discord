ab1bcd85e68afa9c9f2069e2601584ed13caeae3
undefinedprojects <undefinedprojects@outlook.com>
2021-04-17 01:28:51 +0200

    Add config.js, which exports a config
    Add config.js, which exports a config, which can be used in other parts
    of the application.
    This config is generated from environment variables and/or default
    values.
    
    Co-authored-by: Yurifag <yurifag@grzb.de>

---
+++ src/config.js
@@ -0,0 +1,53 @@
+"use strict"
+
+/**
+ * @typedef {Object} RedisConfig
+ * @property {string} hostname
+ * @property {number} port
+ * @property {string} db
+ */
+
+/**
+ * @typedef {Object} DiscordConfig
+ * @property {string} token
+ */
+
+/**
+ * @typedef {Object} Config
+ * @property {RedisConfig} redisConfig
+ */
+
+
+/**
+ * Create a config from environment variables.
+ * @private
+ * @returns {Config} A valid config.
+ */
+function createConfig() {
+    return {
+        redisConfig: {
+            hostname: process.env.REDIS_HOSTNAME || "localhost",
+            port: parseInt(process.env.REDIS_PORT) || 6379,
+            db: process.env.REDIS_DB || 0,
+        },
+        discordConfig: {
+            accessToken: process.env.DISCORD_TOKEN || undefined
+        }
+    };
+}
+
+/**
+ * Checks if the given config object has required values set.
+ * @private
+ * @param {Config} config
+ * @returns {Boolean}
+ */
+function checkConfig(config) {
+    if (config.discordConfig.accessToken === undefined) {
+        throw new Error("You need to provide a Discord token via the DISCORD_TOKEN environment variable.")
+    }
+}
+
+const config = createConfig();
+checkConfig(config);
+module.exports = config;
