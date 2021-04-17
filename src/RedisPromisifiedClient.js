83d91c77a9f7e51c25edb631ecbe514b1dd4421d
undefinedprojects <undefinedprojects@outlook.com>
2021-04-17 02:22:24 +0200

    Add RedisPromisifiedClient
    Add RedisPromisifiedClient, which wraps some methods we need using
    promisify.
    Also add an instance of this client to app.js.
    
    Co-authored-by: Yurifag <yurifag@grzb.de>

---
+++ src/RedisPromisifiedClient.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const { promisify } = require("util");
+const redis = require("redis");
+
+module.exports = exports = class RedisPromisifiedClient {
+    /**
+     * @param {redis.ClientOpts} config
+     */
+    constructor(config) {
+        this.originalClient = redis.createClient(config);
+
+        this.set = promisify(this.originalClient.set).bind(this.originalClient);
+        this.get = promisify(this.originalClient.get).bind(this.originalClient);
+        this.del = promisify(this.originalClient.del).bind(this.originalClient);
+        this.rpush = promisify(this.originalClient.rpush).bind(this.originalClient);
+    }
+}
--- src/app.js
+++ src/app.js
@@ -4,16 +4,19 @@ const Discord = require("discord.js");
 
 const { version } = require("../package.json");
 const config = require("./config");
+const RedisPromisifiedClient = require("./RedisPromisifiedClient");
 
-const client = new Discord.Client();
-client.login(config.token);
+const redisClient = new RedisPromisifiedClient(config.redisConfig);
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`);
-    client.user.setActivity(`Bot Version ${version}`);
+const discordClient = new Discord.Client();
+discordClient.login(config.token);
+
+discordClient.on("ready", () => {
+    console.log(`Logged in as ${discordClient.user.tag}`);
+    discordClient.user.setActivity(`Bot Version ${version}`);
 });
 
-client.on("message", async msg => {
+discordClient.on("message", async msg => {
     if(msg.channel.nsfw && !msg.author.bot) {
         await msg.channel.send("Hello World o/");
     }
