83d9cad8a97d067d6093a7ec8c86a72d10c16c8d
undefinedprojects <undefinedprojects@outlook.com>
2021-04-18 14:25:13 +0200

    Fix "!decensor bar" alias not working for npm package
    Apparently symlinks don't get packaged with npm packages, so don't use a
    symlink for the alias.
    
    Also bump version to 0.2.2.

--- src/commands/decensor/bar.js
+++ src/commands/decensor/bar.js
@@ -1,1 +1,8 @@
-./auto/bar.js
+"use strict";
+
+const command = require("./auto/bar.js");
+
+exports.name = command.name;
+exports.description = command.description;
+exports.execute = command.execute;
+exports.help = command.help;
